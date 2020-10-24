b68898f62b338e46bf3095dd1ea347c30a705bd6
EctoOne <30272930+EctoOne@users.noreply.github.com>
2020-10-24 09:42:24 +0200

    Update rpi-desk.sh
    complete rewrite

--- rpi-desk.sh
+++ rpi-desk.sh
@@ -1,42 +1,103 @@
 #!/bin/bash
 
-trap '' 2 # ignore control + c
-while true
-do
-	clear # clear screen for each loop of menu
-	echo "================="
-	echo "Menu ---"
-	echo  "================="
-	echo "--- Update"
-	echo "	Enter 1 to Update the System"
-	echo "--- XFCE4"
-	echo "	Enter 2 to install a basic XFCE4 desktop"
-	echo "--- Additional Software"
-	echo "	Enter 3 to install Additional Software"
-	echo "--- Dark Theme"
-	echo "	Enter 4 to install dark theme packages"
-	echo "--- Reboot"
-	echo "	Enter r to reboot "
-	echo "--- Quit"
-	echo "	Enter q to quit the script"
-	echo ""
-	echo -e "Enter your selection here and hit <return>"
-	echo ""
-	read answer # create variable to retains the answer
-		case "$answer" in
-		1) sudo apt -y update && sudo apt -y upgrade && sudo apt -y dist-upgrade && sudo apt clean;;
-		2) sudo apt -y install --no-install-recommends xserver-xorg
-            	   sudo apt -y install xfce4 xfce4-terminal lightdm lightdm-gtk-greeter-settings xfce4-datetime-plugin xfce4-netload-plugin xfce4-taskmanager xfce4-appmenu-plugin xfce4-power-manager thunar-archive-plugin thunar-gtkhash evince menulibre mousepad pragha ristretto synaptic gvfs-backends gvfs-fuse
-		   sudo apt clean;;
-		3) sudo apt -y install chromium-browser claws-mail doublecmd-gtk geany gimp guake synapse vlc
-		   sudo apt clean;;
-		4) sudo apt -y install materia-gtk-theme
-		   sudo apt clean
-		   wget -qO- https://raw.githubusercontent.com/gusbemacbe/suru-plus-aspromauros/master/install.sh | sh;;
-		r) sudo reboot;;
-		q) exit;;
-		esac
-			echo ""
-			echo -e "Hit the <return> key to continue"
-			read input ##This causes a pause before the loop clears the screen
-done
+export NEWT_COLORS='
+root=white,black
+border=white,black
+window=white,black
+title=white,black
+textbox=white,black
+compactbutton=lightgray,black
+listbox=white,black
+actlistbox=lightgray,black
+actsellistbox=black,lightgray
+checkbox=white,black
+actcheckbox=black,lightgray
+'
+
+function deskMenu() {
+    DESKTOP=$(whiptail --clear --title "Desktop Environment" --radiolist "Choose desktop:" --cancel-button "Back" --notags 20 75 8 \
+		"lxde" " LXDE desktop " OFF \
+		"xfce" " XFCE desktop " ON 3>&1 1>&2 2>&3
+    )
+	case $DESKTOP in
+		lxde)
+			whiptail --title "Error!" --msgbox "This feature is not implemented yet. Hit OK to continue." 8 78
+			advancedMenu
+		;;
+		xfce)
+			sudo apt -y install --no-install-recommends xserver-xorg
+			sudo apt -y install xfce4 xfce4-terminal lightdm lightdm-gtk-greeter-settings xfce4-datetime-plugin xfce4-netload-plugin xfce4-taskmanager xfce4-appmenu-plugin xfce4-power-manager thunar-archive-plugin thunar-gtkhash evince menulibre mousepad pragha ristretto synaptic gvfs-backends gvfs-fuse
+			sudo apt clean
+			advancedMenu
+		;;
+	esac
+		advancedMenu
+}
+
+function softMenu() {
+    SOFTWARE=$(whiptail --clear --title "Additional Software" --checklist "Select packages:" --cancel-button "Back" 15 75 8 \
+		"chromium-browser" " Webbrowser " ON \
+		"claws-mail" " eMail Client " ON \
+		"doublecmd-gtk" " File manager with two panels " OFF \
+		"geany" " Programmer's text editor " ON \
+		"gimp" " Image editor " ON \
+		"guake" " Top-down terminal " ON \
+		"synapse" " Graphical launcher " ON \
+		"vlc" " Multimedia player " ON 3>&1 1>&2 2>&3
+    )
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+		sudo apt-get -y install ${SOFTWARE//\"}
+		advancedMenu
+    else
+		advancedMenu
+    fi
+}
+
+function styleMenu() {
+    whiptail --clear --title "Appearance Packages" --checklist "Select packages:" --cancel-button "Back" --notags --separate-output 20 75 8 \
+		"materia" " materia-gtk-theme " OFF \
+		"suru" " Suru++ Asprómauros Icons " ON 2>results
+
+    while read choice
+	do
+		case $choice in
+                materia) 
+                sudo apt -y install materia-gtk-theme
+                ;;
+                suru)
+                wget -qO- https://raw.githubusercontent.com/gusbemacbe/suru-plus-aspromauros/master/install.sh | sh
+                ;;
+        esac
+	done < results
+	rm results
+	advancedMenu
+}
+
+function advancedMenu() {
+	ADVSEL=$(whiptail --clear --title "rpi-desk" --menu "Choose an option:" --cancel-button "Exit" --notags 15 75 5 \
+		"1" "Start Raspi-Config" \
+		"2" "Start Update" \
+		"3" "Select Desktop Environment" \
+		"4" "Select Additional Software" \
+		"5" "Select Appearance Packages" 3>&1 1>&2 2>&3
+	)
+	case $ADVSEL in
+		1)
+			sudo raspi-config
+			advancedMenu
+		;;
+		2)
+			sudo apt update && sudo apt -y upgrade
+			advancedMenu
+		;;
+		3) deskMenu
+		;;
+		4) softMenu
+		;;
+		5) styleMenu
+		;;
+	esac
+}
+advancedMenu
+exit
